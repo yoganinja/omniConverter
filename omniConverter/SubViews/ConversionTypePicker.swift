460d63cb96bea43ce0cfbb116cfa4cd0db1370ee
John Florian <developer@johnflorian.com>
2025-01-02 13:13:19 -0800

    Removes unused code.

--- omniConverter/SubViews/ConversionTypePicker.swift
+++ omniConverter/SubViews/ConversionTypePicker.swift
@@ -1,40 +1,40 @@
+////
+////  ConversionTypePicker.swift
+////  omniConverter
+////
+////  Created by John Florian on 12/27/24.
+////
 //
-//  ConversionTypePicker.swift
-//  omniConverter
+//import SwiftUI
 //
-//  Created by John Florian on 12/27/24.
-//
-
-import SwiftUI
-
-struct ConversionTypePicker: View {
-  @Binding var isOpen: Bool
-  @Binding var selectedConversionType: ConversionType
-  let onSelection: (ConversionType) -> Void
-  
-  var body: some View {
-    Button(action: {
-      isOpen.toggle()
-    }) {
-      HStack(alignment: .center) {
-        Image(systemName: selectedConversionType.imageName)
-          .resizable()
-          .frame(width: 24, height: 24)
-          .padding(.vertical)
-        Text(selectedConversionType.id)
-          .font(.system(size: 32, weight: .bold))
-          .padding(.top, 16)
-          .padding(.bottom, 8)
-        
-        Spacer()
-        Image(systemName: "chevron.down")
-          .resizable()
-          .frame(width: 24, height: 24)
-          .padding(.vertical, 40)
-      }
-      .padding(.vertical, 8)
-    }
-    .background(Color.white)
-    .cornerRadius(8)
-  }
-}
+//struct ConversionTypePicker: View {
+//  @Binding var isOpen: Bool
+//  @Binding var selectedConversionType: ConversionType
+//  let onSelection: (ConversionType) -> Void
+//  
+//  var body: some View {
+//    Button(action: {
+//      isOpen.toggle()
+//    }) {
+//      HStack(alignment: .center) {
+//        Image(systemName: selectedConversionType.imageName)
+//          .resizable()
+//          .frame(width: 24, height: 24)
+//          .padding(.vertical)
+//        Text(selectedConversionType.id)
+//          .font(.system(size: 32, weight: .bold))
+//          .padding(.top, 16)
+//          .padding(.bottom, 8)
+//        
+//        Spacer()
+//        Image(systemName: "chevron.down")
+//          .resizable()
+//          .frame(width: 24, height: 24)
+//          .padding(.vertical, 40)
+//      }
+//      .padding(.vertical, 8)
+//    }
+//    .background(Color.white)
+//    .cornerRadius(8)
+//  }
+//}
--- omniConverter/SubViews/SelectionModal.swift
+++ omniConverter/SubViews/SelectionModal.swift
@@ -1,28 +1,28 @@
+////
+////  SelectionModal.swift
+////  omniConverter
+////
+////  Created by John Florian on 12/28/24.
+////
 //
-//  SelectionModal.swift
-//  omniConverter
+//import SwiftUI
 //
-//  Created by John Florian on 12/28/24.
-//
-
-import SwiftUI
-
-struct SelectionModal: View {
-  @Binding var isOpen: [Bool]
-  @Binding var searchQuery: String
-  
-  var body: some View {
-    HStack {
-      TextField("Search...", text: $searchQuery)
-        .textFieldStyle(RoundedBorderTextFieldStyle())
-        .padding(.horizontal)
-        .padding(.top)
-      Button("Close") {
-        for index in isOpen.indices {
-          isOpen[index] = false
-        }
-      }
-      .padding(.trailing)
-    }
-  }
-}
+//struct SelectionModal: View {
+//  @Binding var isOpen: [Bool]
+//  @Binding var searchQuery: String
+//  
+//  var body: some View {
+//    HStack {
+//      TextField("Search...", text: $searchQuery)
+//        .textFieldStyle(RoundedBorderTextFieldStyle())
+//        .padding(.horizontal)
+//        .padding(.top)
+//      Button("Close") {
+//        for index in isOpen.indices {
+//          isOpen[index] = false
+//        }
+//      }
+//      .padding(.trailing)
+//    }
+//  }
+//}
