5536f7dc620e1558118924b5f5e7df8a219388ec
John Florian <developer@johnflorian.com>
2025-01-02 17:37:07 -0800

    Adds Japanese area units. Adds Cent area unit.

--- omniConverter/Dimensions/Area.swift
+++ omniConverter/Dimensions/Area.swift
@@ -23,6 +23,14 @@ enum Area: String, CaseIterable, Identifiable {
   case squareMillimeters = "Square Millimeters"
   case squareNanometers = "Square Nanometers"
   case squareYards = "Square Yards"
+  case cents = "Cents"
+  case shaku = "Shaku"
+  case go = "Gō"
+  case jo = "Jō"
+  case tsubo = "Tsubo"
+  case se = "Se"
+  case tan = "Tan"
+  case cho = "Chō"
   
   var id: String { self.rawValue }
   
@@ -81,6 +89,22 @@ extension UnitArea {
         dict[type.rawValue] = .squareNanometers
       case .squareYards:
         dict[type.rawValue] = .squareYards
+      case .cents:
+        dict[type.rawValue] = .cents
+      case .tsubo:
+        dict[type.rawValue] = .tsubo
+      case .shaku:
+        dict[type.rawValue] = .shaku
+      case .go:
+        dict[type.rawValue] = .go
+      case .jo:
+        dict[type.rawValue] = .jo
+      case .se:
+        dict[type.rawValue] = .se
+      case .tan:
+        dict[type.rawValue] = .tan
+      case .cho:
+        dict[type.rawValue] = .cho
       }
     }
   }()
@@ -98,5 +122,63 @@ extension String {
     }
     
     return nil
+  }
+}
+
+extension UnitArea {
+  static var cents: UnitArea {
+    // 1 cent = 40.46856 square meters
+    return UnitArea(
+      symbol: "cent",
+      converter: UnitConverterLinear(coefficient: 40.46856))
+  }
+  
+  static var shaku: UnitArea {
+    // 1 shaku = 4/121 square meters
+    return UnitArea(
+      symbol: "勺",
+      converter: UnitConverterLinear(coefficient: 4/121))
+  }
+  
+  static var go: UnitArea {
+    // 1 go = 40/121 square meters
+    return UnitArea(
+      symbol: "合",
+      converter: UnitConverterLinear(coefficient: 40/121))
+  }
+  
+  static var jo: UnitArea {
+    // 1 jo = 200/121 square meters
+    return UnitArea(
+      symbol: "帖",
+      converter: UnitConverterLinear(coefficient: 200/121))
+  }
+  
+  static var tsubo: UnitArea {
+    // 1 tsubo = 400/121 square meters
+    return UnitArea(
+      symbol: "坪",
+      converter: UnitConverterLinear(coefficient: 400/121))
+  }
+  
+  static var se: UnitArea {
+    // 1 tsubo = 12000/121 square meters
+    return UnitArea(
+      symbol: "畝",
+      converter: UnitConverterLinear(coefficient: 12000/121))
+  }
+  
+  static var tan: UnitArea {
+    // 1 tsubo = 120000/121 square meters
+    return UnitArea(
+      symbol: "反",
+      converter: UnitConverterLinear(coefficient: 120000/121))
+  }
+  
+  static var cho: UnitArea {
+    // 1 tsubo = 1200000/121 square meters
+    return UnitArea(
+      symbol: "町",
+      converter: UnitConverterLinear(coefficient: 1200000/121))
   }
 }
