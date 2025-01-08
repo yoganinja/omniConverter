916d42cdd5a6aeac4a2e1905969b54288a5ac8c2
John Florian <developer@johnflorian.com>
2025-01-08 09:53:37 -0800

    Enables sorting Favorites by ascending or descending order.

--- omniConverter/SubViews/FavoritesPicker.swift
+++ omniConverter/SubViews/FavoritesPicker.swift
@@ -9,28 +9,38 @@ import SwiftUI
 
 struct FavoritesPicker: View {
   @EnvironmentObject var appState: AppState
+  @State private var isAscending = true
   
   var onSelect: (FavoriteConversion?) -> Void
   
   var body: some View {
     NavigationView {
-      List {
-        ForEach(appState.favoriteConversions.sorted(by: {
-          if $0.conversionType.id != $1.conversionType.id {
-            return $0.conversionType.id < $1.conversionType.id
-          } else if $0.inputUnit != $1.inputUnit {
-            return $0.inputUnit < $1.inputUnit
-          } else {
-            return $0.outputUnit < $1.outputUnit
+      VStack {
+        Button(action: {
+          isAscending.toggle()
+        }) {
+          HStack {
+            Image(systemName: isAscending ? "arrow.up" : "arrow.down")
+            Text("Sort \(isAscending ? "Ascending" : "Descending")")
           }
-        }), id: \.self) { favorite in
-          Button(action: {
-            onSelect(favorite)
-          }) {
-            HStack {
-              Text("\(favorite.conversionType.id)")
-              Spacer()
-              Text("\(favorite.inputUnit) → \(favorite.outputUnit)")
+          .padding()
+          .background(Capsule().fill(Color(.systemGray4)))
+        }
+        .padding()
+        
+        List {
+          ForEach(
+            sortedFavorites(),
+            id: \.self
+          ) { favorite in
+            Button(action: {
+              onSelect(favorite)
+            }) {
+              HStack {
+                Text("\(favorite.conversionType.id)")
+                Spacer()
+                Text("\(favorite.inputUnit) → \(favorite.outputUnit)")
+              }
             }
           }
         }
@@ -42,6 +52,24 @@ struct FavoritesPicker: View {
             onSelect(nil)
           }
         }
+      }
+    }
+  }
+  
+  private func sortedFavorites() -> [FavoriteConversion] {
+    appState.favoriteConversions.sorted {
+      if $0.conversionType.id != $1.conversionType.id {
+        return isAscending
+        ? $0.conversionType.id < $1.conversionType.id
+        : $0.conversionType.id > $1.conversionType.id
+      } else if $0.inputUnit != $1.inputUnit {
+        return isAscending
+        ? $0.inputUnit < $1.inputUnit
+        : $0.inputUnit > $1.inputUnit
+      } else {
+        return isAscending
+        ? $0.outputUnit < $1.outputUnit
+        : $0.outputUnit > $1.outputUnit
       }
     }
   }
